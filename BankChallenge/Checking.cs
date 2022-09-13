79644a29a212ac220b45e47012e9d1cd55618ecb
Evan Reen <74310324+EvanReen@users.noreply.github.com>
2022-09-13 12:12:57 +0100

    Add files via upload

---
+++ BankChallenge/Checking.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankChallenge
+{
+    public class Checking : BankAccount
+    {
+        private const decimal OVERDRAW_CHARGE = 35.0m;
+
+        public Checking(string fname, string lname, decimal initial)
+            : base(fname, lname, initial)
+        {
+        }
+
+        public override void Withdraw(decimal amount)
+        {
+            // if the account gets overdrawn add an extra charge
+            if (amount > Balance)
+            {
+                amount += OVERDRAW_CHARGE;
+            }
+            base.Withdraw(amount);
+        }
+    }
+}
+
